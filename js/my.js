0394a10dda1ccba61997f7b1c29fe2fdcee3440d
Alex Wahlberg <alex.wahlberg@egov.com>
2019-02-07 09:19:10 -0600

    Update my.js

--- js/my.js
+++ js/my.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('[canvas="container"]').animate({
+      $('.wrap').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
 
