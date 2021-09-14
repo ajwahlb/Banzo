a0152b3b54c4204a0431b0beeeacd9808f26fa12
Alex Wahlberg <alexjwahlberg@gmail.com>
2021-09-13 20:07:25 -0500

    Update my.js

--- js/my.js
+++ js/my.js
@@ -5,7 +5,39 @@ $('.fly-in').click(function(){
 	$('.nav-bg').toggleClass('show');
 });
 
+// var topofDiv = $(".full-height").offset().top;
+// var height = $(".full-height").outerHeight();
+//
+// $(window).scroll(function(){
+//     if($(window).scrollTop() > (topofDiv + height - 100)){
+//        $("#navbar").addClass('navbar-fixed');
+//     }
+//     else{
+//        $("#navbar").removeClass('navbar-fixed');
+//     }
+// });
 
- $( '.toggle-slidebar-1' ).on( 'click', function () {
-	 $( '.slidebar-1' ).toggle();
- } );
+// left and right slide in doodads
+( function ( $ ) {
+	// Initialize Slidebars
+	var controller = new slidebars();
+	controller.init();
+	// Toggle Slidebars
+	 $( '.toggle-slidebar-1' ).on( 'click', function ( event ) {
+		 // Stop default action and bubbling
+		 event.stopPropagation();
+		 event.preventDefault();
+
+		 // Toggle the Slidebar with id 'slidebar-1'
+		 controller.toggle( 'slidebar-1' );
+	 } );
+	// Toggle Slidebars
+	 $( '.toggle-slidebar-2' ).on( 'click', function ( event ) {
+		 // Stop default action and bubbling
+		 event.stopPropagation();
+		 event.preventDefault();
+
+		 // Toggle the Slidebar with id 'slidebar-2'
+		 controller.toggle( 'slidebar-2' );
+	 } );
+} ) ( jQuery );
