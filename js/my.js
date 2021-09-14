084aba6c7b3816040beda077680c9f705c7fdaf2
Alex Wahlberg <alexjwahlberg@gmail.com>
2021-09-13 19:58:59 -0500

    Update my.js

--- js/my.js
+++ js/my.js
@@ -4,3 +4,9 @@ $('.fly-in').click(function(){
 	$('.nav-container').toggleClass('slide');
 	$('.nav-bg').toggleClass('show');
 });
+
+
+ $( '.toggle-slidebar-1' ).on( 'click', function ( event ) {
+	 controller.toggle( 'slidebar-1' );
+ } );
+	
