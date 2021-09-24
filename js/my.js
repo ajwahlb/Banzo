d93e50ba5d3fd980e79fd0efc44c5d0b4962ef60
Alex Wahlberg <alexjwahlberg@gmail.com>
2021-09-23 22:03:25 -0500

    fixes

--- js/my.js
+++ js/my.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('input[type="radio"]').click(function(){
     var inputValue = $(this).attr("value");
     var targetBox = $("." + inputValue);
-    $(".box").not(targetBox).hide();
+    $(".row").not(targetBox).hide();
     $(targetBox).show();
   });
 });
